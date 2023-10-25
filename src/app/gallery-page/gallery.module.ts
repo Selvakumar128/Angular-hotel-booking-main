3744b48c9f2f17a02e19917072c45c7d048b3ab9
Selvakumar128 <selvakumar4183131@gmail.com>
2023-10-25 12:26:39 +0530

    abc

---
+++ src/app/gallery-page/gallery.module.ts
@@ -0,0 +1,10 @@
+// import { NgModule } from '@angular/core';
+// import { GalleryPageComponent } from './gallery-page.component';
+// import { NavbarComponent } from '../navbar/navbar.component';
+
+
+// @NgModule({
+//   declarations: [GalleryPageComponent],
+// })
+// export class GalleryModule { }
+
